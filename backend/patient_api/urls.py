3d171f306637e539cf5522689763e29304ee2985
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-03-12 19:30:44 +0600

    feat: patient profile api

--- backend/patient_api/urls.py
+++ backend/patient_api/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from .views.token import MyTokenObtainPairView
 from .views.register import UserRegistrationView
+from .views.profile import PatientProfileView
 
 
 urlpatterns = [
     # POST: localhost:8000/api/patient/register/
-    path(route="register/", view=UserRegistrationView.as_view(), name="token_obtain_pair"),
+    path(route="register/", view=UserRegistrationView.as_view(), name="patient_register"),
     # POST: localhost:8000/api/patient/login/
-    path(route="login/", view=MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(route="login/", view=MyTokenObtainPairView.as_view(), name="patient_login"),
+    # GET: localhost:8000/api/patient/profile/
+    path(route="profile/", view=PatientProfileView.as_view(), name="patient_profile"),
 ]
