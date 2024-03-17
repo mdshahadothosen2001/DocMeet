b48e0fadf69ecd86d5ad41f6f52e49f71be83277
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-03-17 11:13:55 +0600

    feat: book appointment api created

--- backend/appointment_api/urls.py
+++ backend/appointment_api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from .views.appointment import AppointmentListView
+from .views.book import BookAppointmentView
 
 
 
 urlpatterns = [
     # GET: localhost:8000/api/appointment/list/
     path(route="list/", view=AppointmentListView.as_view(), name="appointment_list"),
+    # POST: localhost:8000/api/appointment/book/
+    path(route="book/", view=BookAppointmentView.as_view(), name="appointment_book"),
 ]
---
+++ backend/appointment_api/views/book.py
@@ -0,0 +1,39 @@
+from rest_framework.status import HTTP_202_ACCEPTED, HTTP_400_BAD_REQUEST
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..serializers.book import BookAppointmentSerializer
+from book_appointment.models import BookAppointmentModel
+
+
+class BookAppointmentView(APIView):
+    """User can book any appointment."""
+
+    permission_classes = [IsAuthenticated]
+
+    def validate_parameter(self, patient_id, appointment_id):
+        if patient_id and appointment_id:
+            return True
+        else:
+            return False
+
+    def post(self, request, *args, **kwargs):
+        patient_id = request.user.id
+        appointment_id = request.data.get("appointment_id")
+
+        if self.validate_parameter(patient_id, appointment_id) is True:
+            book_instance = BookAppointmentModel.objects.filter(appointment=appointment_id).exists()
+            if book_instance is False:
+                book_appointment = {
+                    "patient": patient_id,
+                    "appointment": appointment_id,
+                }
+
+                serializer = BookAppointmentSerializer(data=book_appointment)
+                if serializer.is_valid():
+                    serializer.save()
+
+                    return Response({"message":"Send your appointment request!"}, status=HTTP_202_ACCEPTED)
+
+        return Response({"message":"Incompleted request! Please provide valid data"}, status=HTTP_400_BAD_REQUEST)
