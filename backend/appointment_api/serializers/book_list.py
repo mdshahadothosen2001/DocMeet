4aec75898b451b6e8e3467f7b339ac0788e055dc
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-03-17 11:59:45 +0600

    chore: updated book list api

--- backend/appointment_api/serializers/book_list.py
+++ backend/appointment_api/serializers/book_list.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
 
 from book_appointment.models import BookAppointmentModel
+from appointment.models import AppointmentModel
 from user.models import UserAccount
 
+
+class DoctorDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserAccount
+        fields = [
+            "id",
+            "first_name", 
+        ]
+    
+class AppointmentListSerializer(serializers.ModelSerializer):
+
+    doctor_detail = DoctorDetailSerializer(source='doctor', read_only=True)
+
+    class Meta:
+        model = AppointmentModel
+        fields = [
+            "id",
+            "doctor_detail",
+            "day", 
+            "date", 
+            "start_time",
+            "end_time",
+            "availability",
+            ]
 
 class PatientDetailSerializer(serializers.ModelSerializer):
 
@@ -16,11 +42,13 @@ class PatientDetailSerializer(serializers.ModelSerializer):
 class BookAppointmentListSerializer(serializers.ModelSerializer):
 
     patient_detail = PatientDetailSerializer(source='patient', read_only=True)
+    appointment_detail = AppointmentListSerializer(source='appointment', read_only=True)
 
     class Meta:
         model = BookAppointmentModel
         fields = [
             "id",
             "patient_detail",
+            "appointment_detail",
             "is_complete", 
             ]
