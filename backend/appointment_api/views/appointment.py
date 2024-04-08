bb130f5b1a93141dd9618b30742f40b905e78e05
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-04-08 16:39:37 +0600

    chore: appointment list api modified

--- backend/appointment_api/views/appointment.py
+++ backend/appointment_api/views/appointment.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 
 from appointment.models import AppointmentModel
-
+from user.models import Doctor
 from ..serializers.appointment import AppointmentListSerializer
 
 
@@ -12,6 +12,18 @@ class AppointmentListView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
+        specialized_id = request.query_params.get("specialized_id")
+        doctors = Doctor.objects.values()
+        if specialized_id:
+            doctors = doctors.filter(specialization=specialized_id)
+            appointment_list = []
+            for doctor in doctors:
+                appointments =AppointmentModel.objects.filter(doctor=doctor.get("id"))
+                serializer = AppointmentListSerializer(appointments, many=True)
+                appointment_list += serializer.data
+            return Response(appointment_list, status=HTTP_200_OK)
+
+
         appointments =AppointmentModel.objects.all()
         serializer = AppointmentListSerializer(appointments, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
