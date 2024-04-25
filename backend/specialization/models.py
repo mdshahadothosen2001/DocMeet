4d8ca7f91487b32ee9adf444b60bee19f5719dc5
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-04-25 09:30:23 +0600

    chore: specialization model modified

--- backend/specialization/models.py
+++ backend/specialization/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class SpecializationModel(models.Model):
+    ordering_id = models.PositiveSmallIntegerField(unique=True, null=True, blank=True)
     name = models.CharField(max_length=100, unique=True)
     picture = models.URLField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
