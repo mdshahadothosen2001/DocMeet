8169830529e090b43f7e96f815e9b1106fa68b8d
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-05-07 20:24:21 +0600

    chore: book list api modified

--- backend/doctor_api/serializers/book_list.py
+++ backend/doctor_api/serializers/book_list.py
@@ -1,57 +1,17 @@
 from rest_framework import serializers
-
 from book_appointment.models import BookAppointmentModel
-from appointment.models import AppointmentModel
 from user.models import UserAccount
 
-
-class DoctorDetailSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UserAccount
-        fields = [
-            "id",
-            "first_name", 
-            "last_name", 
-        ]
-    
-class AppointmentListSerializer(serializers.ModelSerializer):
-
-    doctor_detail = DoctorDetailSerializer(source='doctor', read_only=True)
-
-    class Meta:
-        model = AppointmentModel
-        fields = [
-            "id",
-            "doctor_detail",
-            "day", 
-            "date", 
-            "start_time",
-            "end_time",
-            "availability",
-            ]
-
-class PatientDetailSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UserAccount
-        fields = [
-            "id",
-            "first_name",
-            "last_name",
-            "phone_number",
-        ]
-    
 class BookAppointmentListSerializer(serializers.ModelSerializer):
-
-    patient_detail = PatientDetailSerializer(source='patient', read_only=True)
-    appointment_detail = AppointmentListSerializer(source='appointment', read_only=True)
+    phone_number = serializers.CharField(source='patient.phone_number', read_only=True)
+    email = serializers.CharField(source='patient.email', read_only=True)
+    first_name = serializers.CharField(source='patient.first_name', read_only=True)
+    last_name = serializers.CharField(source='patient.last_name', read_only=True)
+    day = serializers.CharField(source='appointment.day', read_only=True)
+    date = serializers.CharField(source='appointment.date', read_only=True)
+    start_time = serializers.CharField(source='appointment.start_time', read_only=True)
+    end_time = serializers.CharField(source='appointment.end_time', read_only=True)
 
     class Meta:
         model = BookAppointmentModel
-        fields = [
-            "id",
-            "patient_detail",
-            "appointment_detail",
-            "is_complete", 
-            ]
+        fields = ["id", "phone_number", "email", "first_name", "last_name", "day", "date", "start_time", "end_time", "is_complete"]
